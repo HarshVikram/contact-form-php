c63625148a160f8c0115709217e1870a41fe2a13
HarshVikram <harsh.vikram34@gmail.com>
2021-05-10 17:14:19 +0530

    variables assigned in php file

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
-
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        
+        // Assign Variables
+        $user = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+        $mail = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+        $cell = filter_var($_POST['cellphone'], FILTER_SANITIZE_NUMBER_INT);
+        $msg  = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+                
+        $headers = 'From: ' . $mail . '\r\n';
+        $myEmail = 'osama.elzero@gmail.com';
+        $subject = 'Contact Form';
+        
+        if (empty($formErrors)) {
+            
+            mail($myEmail, $subject, $msg, $headers);
+            
+            $user = '';
+            $mail = '';
+            $cell = '';
+            $msg = '';
+            
+            $success = '<div class="alert alert-success">We Have Recieved Your Message</div>';
+            
+        }
+        
+    }
 ?>
