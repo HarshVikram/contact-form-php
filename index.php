a7ceca6ba94020d00fdaf6131b517af7c61ceb82
HarshVikram <harsh.vikram34@gmail.com>
2021-05-10 17:49:40 +0530

    .gitattributes and .gitignore added

--- index.php
+++ index.php
@@ -121,5 +121,8 @@
         </div>
         
         <!-- End Form -->
+        <script src="js/jquery-1.12.4.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/custom.js"></script>
     </body>
 </html>
